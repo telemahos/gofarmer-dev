6eff55f3772f284b39b43467cd9a314dda57e4d0
telemahos <kakoulis@hotmail.com>
2012-11-21 23:50:20 +0200

    some ignore files deleted

--- bonfire/themes/gf/cache/gf_questions_gf_my_profile_mod.js
+++ bonfire/themes/gf/cache/gf_questions_gf_my_profile_mod.js
@@ -48,48 +48,6 @@ $('#subjectRemainingCharacters').html(subjectCharacterLimit);
 
     // TimeAgo plugin 
     jQuery("abbr.timeago").timeago();
-
-/*if( !('crop_comment' in CKEDITOR.instances)) {
-	CKEDITOR.replace( 'crop_comment' );
-}*/
-
-	// AJAX CALL FOR SELECT BOX
-    $("#variety").attr('disabled','disabled');
-    // Add the disable state of the button
-    $("input#crop_sumbit").addClass("disabled");
-    $("input#crop_sumbit").attr('disabled','disabled');
-
-	$("select#crop_crops").change( function() {		
-		// Remove the disable state of the button
-		$("input#crop_sumbit").removeClass("disabled"); 
-		$("input#crop_sumbit").removeAttr('disabled','disabled');
-		$("#variety").attr('disabled','disabled');
-		//$("#result").html("<img src='../images/ajax-loader.gif' width='' height='' />");
-		$.ajax({
-			type: "POST",
-			data: "data=" + $("select#crop_crops").val(),
-			url:  "get_crop_var_list",
-			success: function(msg){
-				if (msg != ''){
-					if ($("select#crop_crops").val() == 0) {
-						// Add the disable state of the button
-						$("input#crop_sumbit").addClass("disabled");
-						$("input#crop_sumbit").attr('disabled','disabled');
-						$("#variety").val(0);
-						$("#variety").attr('disabled','disabled');
-						$("#result").html('');
-					}
-					else {
-						$("#variety").removeAttr('disabled');
-						$("#variety").html(msg);
-					}
-				}
-				else{
-					$("#result").html('<em>No item result</em>');
-				}
-			}
-		});
-	});
 $('#croffer_release_date').datepicker({ dateFormat: 'd M, yy'});
 // $('#croffer_release_date').datepicker({ dateFormat: 'dd-mm-yy'});
 
