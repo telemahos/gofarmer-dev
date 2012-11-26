533f5a7d05c850440b1ef26709d01e11c45b0249
telemahos <kakoulis@hotmail.com>
2012-11-26 23:53:18 +0200

    add greek in wizard

---
+++ bonfire/modules/wizard/language/greek/wizard_lang.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$lang['wizard_manage']			= 'Manage wizard';
+$lang['wizard_edit']				= 'Edit';
+$lang['wizard_true']				= 'True';
+$lang['wizard_false']				= 'False';
+$lang['wizard_create']			= 'Create';
+$lang['wizard_list']				= 'List';
+$lang['wizard_new']				= 'New';
+$lang['wizard_edit_text']			= 'Edit this to suit your needs';
+$lang['wizard_no_records']			= 'There aren\'t any wizard in the system.';
+$lang['wizard_create_new']			= 'Create a new wizard.';
+$lang['wizard_create_success']			= 'wizard successfully created.';
+$lang['wizard_create_failure']			= 'There was a problem creating the wizard: ';
+$lang['wizard_create_new_button']			= 'Create New wizard';
+$lang['wizard_invalid_id']			= 'Invalid wizard ID.';
+$lang['wizard_edit_success']			= 'wizard successfully saved.';
+$lang['wizard_edit_failure']			= 'There was a problem saving the wizard: ';
+$lang['wizard_delete_success']			= 'record(s) successfully deleted.';
+$lang['wizard_delete_failure']			= 'We could not delete the record: ';
+$lang['wizard_delete_error']			= 'You have not selected any records to delete.';
+$lang['wizard_actions']			= 'Actions';
+$lang['wizard_cancel']			= 'Cancel';
+$lang['wizard_delete_record']			= 'Delete this wizard';
+$lang['wizard_delete_confirm']			= 'Are you sure you want to delete this wizard?';
+$lang['wizard_edit_heading']			= 'Edit wizard';
+
+// Activities
+$lang['wizard_act_create_record']			= 'Created record with ID';
+$lang['wizard_act_edit_record']			= 'Updated record with ID';
+$lang['wizard_act_delete_record']			= 'Deleted record with ID';
